4336f502777fddf20b8edd40979faa91e46ddae2
thanhnguyen195 <thanhnguyen.hampshire@gmail.com>
2022-11-30 21:31:24 +0700

    Initial Commit

---
+++ Sources/DreamerlySdk.xcframework/ios-arm64_x86_64-simulator/DreamerlySdk.framework/Headers/DreamerlySdk.h
@@ -0,0 +1,18 @@
+//
+//  DreamerlySdk.h
+//  DreamerlySdk
+//
+//  Created by thanh nguyen on 11/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DreamerlySdk.
+FOUNDATION_EXPORT double DreamerlySdkVersionNumber;
+
+//! Project version string for DreamerlySdk.
+FOUNDATION_EXPORT const unsigned char DreamerlySdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DreamerlySdk/PublicHeader.h>
+
+
